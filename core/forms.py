0571252f28b1a232747c4f71c48c44f39d615731
bedirhansahin <bedirhannsahin@gmail.com>
2023-03-29 00:05:47 +0300

    Registration page is created

--- core/forms.py
+++ core/forms.py
@@ -6,9 +6,9 @@ from django import forms
 class SignUpForm(UserCreationForm):
     email = forms.EmailField(label="",
                              widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Email Address'}))
-    first_name = forms.CharField(max_length=100,
+    first_name = forms.CharField(label="", max_length=100,
                                  widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'First Name'}))
-    last_name = forms.CharField(max_length=100,
+    last_name = forms.CharField(label="", max_length=100,
                                 widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Last Name'}))
 
     class Meta:
--- core/views.py
+++ core/views.py
@@ -2,11 +2,13 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 
+from core.forms import SignUpForm
+
 
 def login_user(request):
     if request.method == 'POST':
         username = request.POST.get('username', False)
-        password = request.POST['password']
+        password = request.POST.get('password')
 
         user = authenticate(request, username=username, password=password)
         if user:
@@ -27,4 +29,47 @@ def logout_user(request):
 
 
 def register_user(request):
-    return render(request, 'register.html', {})
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, "You Have Successfully Register")
+            return redirect('home')
+    else:
+        form = SignUpForm()
+        return render(request, 'register.html', {'form': form})
+    return render(request, 'register.html', {'form': form})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
